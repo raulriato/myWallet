b05270821b1717da16fee5235437751c4e5f241a
raulriato <raulriato@gmail.com>
2022-09-08 19:23:28 -0300

    feat Button style

---
+++ src/styles/Button.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+    width: 87%;
+    height: 58px;
+    background-color: rgba(163, 40, 214, 1);
+    font-family: 'Raleway', sans-serif;
+    font-weight: 700;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    ${
+        ({ square }) => square ? `
+            width: 156px;
+            height: 114px;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: start;
+            padding-left: 12px;
+        `: ""
+    }
+
+    ${
+        ({ disabled }) => disabled ? `
+            opacity: 0.7;
+            cursor: initial;
+    ` : ''}
+`;
+
+export default Button;
